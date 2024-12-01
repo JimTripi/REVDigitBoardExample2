571fb220c46188f5e8f7064386fad177f7eed0f7
JimTripi <JimTripi@users.noreply.github.com>
2024-12-01 17:35:46 -0500

    Interim save

---
+++ REVDigitBoardExample2/src/main/java/frc/robot/REVDigitBoardController.java
@@ -0,0 +1,35 @@
+package frc.robot;
+
+public class REVDigitBoardController extends REVDigitBoard {
+
+    // public RobotColorEnum m_RobotColor = RobotColorEnum.eRed;
+    // public RobotColorEnum toggleRobotColor() { 
+    //     return (m_RobotColor == RobotColorEnum.eRed) ? RobotColorEnum.eBlue : RobotColorEnum.eRed; 
+    // }
+
+    // public enum RobotStartPositionEnum { eOne, eTwo, eThree };
+    // public RobotStartPositionEnum m_RobotStartPosition = RobotStartPositionEnum.eOne;
+    
+
+    public enum RobotColorEnum { eRed, eBlue };
+    public RobotColorEnum m_RobotColor = RobotColorEnum.eBlue;   //  restrict to R or B (red or blue)
+    public void setRobotColor(RobotColorEnum InColor) {
+        m_RobotColor = (InColor == RobotColorEnum.eRed) ? RobotColorEnum.eBlue : RobotColorEnum.eRed;
+    }
+    public RobotColorEnum getRobotColor() { return (m_RobotColor == RobotColorEnum.eRed) ? RobotColorEnum.eBlue : RobotColorEnum.eRed; }
+    public String getRobotColorString() { return ((m_RobotColor == RobotColorEnum.eRed) ? "R" : "B"); }
+
+    public String m_StartPositionNumber = "1";  // Restrict to characters 1,2,3
+    public String m_ScenarioNumberForPosition = "1";  // Restrict to characters 1,2,3,... for each Start Position.
+
+    public void logRevDigitBoardControllerState() {
+        System.out.println("RevDigitBoardController>" +  
+            "  Color: " + getRobotColorString() + 
+            "  StartPosition: " + m_StartPositionNumber + 
+            "  Scenario: " + m_ScenarioNumberForPosition +
+            "  ButtonA: " + getButtonA() + 
+            "  ButtonB: " + getButtonB() + 
+            "  Pot: " + getAdjustPotentiometerVoltage() );
+    }
+  
+}
---
+++ REVDigitBoardExample2/src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.REVDigitBoardController.RobotColorEnum;
+
+public class RobotContainer {
+
+  //private final REVDigitBoard revDigitBoard = new REVDigitBoard();
+  private final REVDigitBoardController revDigitBoardController = new REVDigitBoardController();
+  private double m_pot = 0;
+
+  public RobotContainer() {
+    configureBindings();
+    revDigitBoardController.clear();
+    revDigitBoardController.display("DEAD");
+  }
+
+  private void configureBindings() {
+
+    Trigger TriggerA = new Trigger(() -> revDigitBoardController.getButtonA());
+    TriggerA.onTrue(new InstantCommand(()->{
+      //revDigitBoardController.setRobotColor(RobotColorEnum.eRed);
+      System.out.println("A Pressed");
+    })).onFalse(new InstantCommand(()->{
+      //revDigitBoardController.setRobotColor(RobotColorEnum.eBlue);
+      System.out.println("A NOT PRESSED");
+    }));
+
+    Trigger TriggerB = new Trigger(() -> revDigitBoardController.getButtonB());
+
+  }
+
+  public void printRevDigitBoardControllerState() {
+    revDigitBoardController.logRevDigitBoardControllerState();
+  }
+
+  public Command getAutonomousCommand() {
+    return Commands.print("No autonomous command configured");
+  }
+}
